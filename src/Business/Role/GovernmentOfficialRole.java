ec4f6b2dd1f7edac75c28857e3e19401e4c162ed
Apurva-Nehru <nehru.a@northeastern.edu>
2021-12-10 12:15:35 -0500

    Added Roles

---
+++ src/Business/Role/GovernmentOfficialRole.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Oragnization.Organization;
+import Business.Oragnization.GovOrg;
+import Business.UserAccount.UserAccount;
+import UserInterface.GovernmentOfficialOrganizationRole.GovernmentOfficialWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author apurv
+ */
+public class GovernmentOfficialRole extends Roles{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new GovernmentOfficialWorkAreaJPanel(userProcessContainer, account, (GovOrg)organization, enterprise, business); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

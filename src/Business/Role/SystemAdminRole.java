0e066daab9cc5c061bf5a5128169acbe1ac18cc7
AishwaryaBalyaya10 <balyaya.a@northeastern.edu>
2021-12-10 12:30:04 -0500

    Changes in Roles

--- src/Business/Role/SystemAdminRole.java
+++ src/Business/Role/SystemAdminRole.java
@@ -1,24 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Business.Role;
 
 import Business.EcoSystem;
-
+import Business.Enterprise.Enterprise;
+import Business.Oragnization.Organization;
 import Business.UserAccount.UserAccount;
-import userinterface.SystemAdminWorkArea.SystemAdminWorkAreaJPanel;
+import UserInterface.SystemAdminWorkarea.SystemAdminWorkAreaJPanel;
 import javax.swing.JPanel;
 
 /**
  *
- * @author raunak
+ * @author aishw
  */
-public class SystemAdminRole extends Role{
-
-  
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem system) {
+public class SystemAdminRole extends Roles{
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system) {
         return new SystemAdminWorkAreaJPanel(userProcessContainer, system);
     }
-    
+
 }

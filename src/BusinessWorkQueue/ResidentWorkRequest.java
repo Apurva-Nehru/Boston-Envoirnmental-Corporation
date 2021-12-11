75da66c8dcbf6ccae4316a870b8280f678cf4908
AishwaryaBalyaya10 <balyaya.a@northeastern.edu>
2021-12-11 01:59:33 -0500

    Changes

--- src/BusinessWorkQueue/ResidentWorkRequest.java
+++ src/BusinessWorkQueue/ResidentWorkRequest.java
@@ -5,10 +5,49 @@
  */
 package BusinessWorkQueue;
 
+import Business.Sensor.AirPollutionSensor;
+import Business.Sensor.FloodManagementSensor;
+import java.util.ArrayList;
 /**
  *
  * @author aishw
  */
-public class ResidentWorkRequest {
+public class ResidentWorkRequest extends WorkRequest{
+    private AirPollutionSensor airPollutionSensor;
+    private ArrayList<FloodManagementSensor> floodManagementSensorList;
+    private String AirPollutionMessage;
+    private String FloodManagementMessage; 
+
+    public AirPollutionSensor getAirPollutionSensor() {
+        return airPollutionSensor;
+    }
+
+    public void setAirPollutionSensor(AirPollutionSensor airPollutionSensor) {
+        this.airPollutionSensor = airPollutionSensor;
+    }
+
+    public String getAirPollutionMessage() {
+        return AirPollutionMessage;
+    }
+
+    public void setAirPollutionMessage(String AirPollutionMessage) {
+        this.AirPollutionMessage = AirPollutionMessage;
+    }
+
+    public String getFloodManagementMessage() {
+        return FloodManagementMessage;
+    }
+
+    public void setFloodManagementMessage(String FloodManagementMessage) {
+        this.FloodManagementMessage = FloodManagementMessage;
+    }
+    
+    public ArrayList<FloodManagementSensor> getFloodManagementSensorList() {
+        return floodManagementSensorList;
+    }
+
+    public void setFloodManagementSensorList(ArrayList<FloodManagementSensor> floodManagementSensorList) {
+        this.floodManagementSensorList = floodManagementSensorList;
+    }
     
 }

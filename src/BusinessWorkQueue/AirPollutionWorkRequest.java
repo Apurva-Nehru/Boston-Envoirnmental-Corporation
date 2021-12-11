73c4ae7922ab8abe85fa839564d15b9082e52adc
AishwaryaBalyaya10 <balyaya.a@northeastern.edu>
2021-12-11 01:35:56 -0500

    Added work Queue

---
+++ src/BusinessWorkQueue/AirPollutionWorkRequest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessWorkQueue;
+
+import Business.Sensor.AirPollutionSensor;
+/**
+ *
+ * @author aishw
+ */
+public class AirPollutionWorkRequest extends WorkRequest{
+    private AirPollutionSensor airPollutionSensor;
+
+    public AirPollutionSensor getAirPollutionSensor() {
+        return airPollutionSensor;
+    }
+
+    public void setAirPollutionSensor(AirPollutionSensor airPollutionSensor) {
+        this.airPollutionSensor = airPollutionSensor;
+    }
+    
+}

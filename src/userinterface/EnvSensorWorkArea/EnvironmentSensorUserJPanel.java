9a621f5ffaa4b0ad9293ca71971c7f7b163ac1b3
Apurva-Nehru <nehru.a@northeastern.edu>
2021-12-11 21:11:42 -0500

    changes

--- src/userinterface/EnvSensorWorkArea/EnvironmentSensorUserJPanel.java
+++ src/userinterface/EnvSensorWorkArea/EnvironmentSensorUserJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UserInterface.EnvSensorWorkArea;
+package userinterface.EnvSensorWorkArea;
 
 import Business.EcoSystem;
 import Business.Employee.Employee;
